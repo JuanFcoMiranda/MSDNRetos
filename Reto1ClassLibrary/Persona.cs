e538c6cac42a57b500883a01e784b748c8426805
Juan Francisco Miranda Aguilar <jfmiranda@gmail.com>
2014-09-08 20:46:38 +0200

    Terminado el reto 1.
    Signed-off-by: Juan Francisco Miranda Aguilar <jfmiranda@gmail.com>

---
+++ Reto1ClassLibrary/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reto1ClassLibrary {
+    public class Persona : IEquatable<Persona> {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public bool Equals(Persona otraPersona) {
+            return Nombre == otraPersona.Nombre && Edad == otraPersona.Edad;
+        }
+
+        public override bool Equals(object obj) {
+            if (obj == null) {
+                return false;
+            }
+
+            var otraPersona = obj as Persona;
+            if (otraPersona == null) {
+                return false;
+            }
+
+            return Nombre == otraPersona.Nombre && Edad == otraPersona.Edad;
+        }
+    }
+}
---
+++ Reto1ClassLibrary/Reto1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Reto1ClassLibrary;
+
+namespace Reto1Lib {
+    public class Reto1 {
+        public static List<Persona> OrdenarLista(List<Persona> lista) {
+            if (lista != null)
+                return (from p in lista.ToList()
+                        orderby p.Edad descending, p.Nombre 
+                    select p).ToList();
+            return null;
+        }
+    }
+}
