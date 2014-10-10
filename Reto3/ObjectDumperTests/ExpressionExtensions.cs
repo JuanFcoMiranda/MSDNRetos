04b67906e5a854a365fa0b3534d07116f8cb0203
Juan Francisco Miranda Aguilar <jfmiranda@gmail.com>
2014-10-10 20:33:41 +0200

    Añadido el reto 3.
    Signed-off-by: Juan Francisco Miranda Aguilar <jfmiranda@gmail.com>

---
+++ Reto3/ObjectDumperTests/ExpressionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ObjectDumperTests {
+    static class ExpressionExtensions {
+        public static PropertyInfo AsPropertyInfo<T, TR>(this Expression<Func<T, TR>> expr) {
+            var memberExp = expr.Body as MemberExpression;
+            if (memberExp == null) {
+                return null;
+            }
+            return memberExp.Member as PropertyInfo;
+        }
+    }
+}
---
+++ Reto3/ObjectDumperTests/ObjectDumper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ObjectDumperTests {
+    public class ObjectDumper<T> where T : class {
+        private readonly Dictionary<string, Delegate> templates;
+
+        public ObjectDumper() {
+            templates = new Dictionary<string, Delegate>();
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> Dump(T testClass) {
+            var type = testClass.GetType();
+                var properties = type.GetProperties().Where(property => property.CanRead).OrderBy(property => property.Name);
+                foreach (var property in properties) {
+                    var template = templates.ContainsKey(property.Name) ? templates[property.Name] : null;
+                    var value = property.GetValue(testClass);
+                    yield return
+                        template != null
+                            ? new KeyValuePair<string, string>(property.Name, template.DynamicInvoke(value) as string)
+                            : new KeyValuePair<string, string>(property.Name, value != null ? value.ToString() : null);
+                }
+        }
+
+        public void AddTemplateFor<TR>(Expression<Func<T, TR>> propExp, Func<TR, string> template) {
+            var property = propExp.AsPropertyInfo();
+            if (property == null) {
+                return;
+            }
+            templates.Add(property.Name, template);
+        }
+    }
+}
