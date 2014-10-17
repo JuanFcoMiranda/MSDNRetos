ddc677b32d1856ebbb632c0379ca86b5a490183e
Juan Francisco Miranda Aguilar <jfmiranda@gmail.com>
2014-10-17 20:10:25 +0200

    Actualizado con la solución al Reto4.
    Signed-off-by: Juan Francisco Miranda Aguilar <jfmiranda@gmail.com>

---
+++ Shuffler/Shuffler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shuffler {
+    public static class Shuffler {
+        public static List<T> Shuffle<T>(this IList<T> lista) {
+            var list = new List<T>(lista);
+            int n = list.Count;
+            Random rnd = new Random();
+            while (n > 1) {
+                int k = rnd.Next(n - 1);
+                T value = list[k];
+                list[k] = list[n-1];
+                list[n-1] = value;
+                if (lista[k].Equals(list[k]))
+                    continue;
+                n--;
+            }
+            return list;
+        }
+    }
+}
